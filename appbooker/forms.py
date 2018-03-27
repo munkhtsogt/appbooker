84e83523da8fa84153930ea60d9f760cb574b457
munkhuu48@gmail.com <munkhuu48@gmail.com>
2018-03-26 20:34:55 -0700

    some improvements

--- appbooker/forms.py
+++ appbooker/forms.py
@@ -2,12 +2,11 @@ from django import forms
 from .models import Appointment
 
 class AppointmentForm(forms.ModelForm):
-	
-    class Meta:
-        model = Appointment
-        fields = ('date', 'time', 'description',)
-        widgets = {
-            'date': forms.DateInput(format=('%b %d'), attrs={'class': 'form-control'}),
-            'time': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.TextInput(attrs={'class': 'form-control'}),
-        }
+	class Meta:
+		model = Appointment
+		fields = ('date', 'time', 'description',)
+		widgets = {
+			'date': forms.TextInput(attrs={'class': 'form-control', 'required': 'true', 'type': 'date'}),
+			'time': forms.TextInput(attrs={'class': 'form-control', 'required': 'true', 'type': 'time'}),
+			'description': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
+		}
--- appbooker/models.py
+++ appbooker/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Appointment(models.Model):
-	date = models.CharField(max_length=10)
-	time = models.CharField(max_length=10)
+	date = models.DateField()
+	time = models.TimeField()
 	description = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
 	
  	def __str__(self):
-		return self.date + " " + self.time
+		return str(self.date) + " " + str(self.time)

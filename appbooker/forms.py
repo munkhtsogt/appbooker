12a8e9f55765597f5fea1ec7b699c2a279942da4
munkhuu48@gmail.com <munkhuu48@gmail.com>
2018-03-26 19:59:08 -0700

    some css improvement to form

--- appbooker/forms.py
+++ appbooker/forms.py
@@ -2,7 +2,12 @@ from django import forms
 from .models import Appointment
 
 class AppointmentForm(forms.ModelForm):
-
+	
     class Meta:
         model = Appointment
         fields = ('date', 'time', 'description',)
+        widgets = {
+            'date': forms.DateInput(format=('%b %d'), attrs={'class': 'form-control'}),
+            'time': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.TextInput(attrs={'class': 'form-control'}),
+        }

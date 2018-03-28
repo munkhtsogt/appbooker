db7f386e452f91e88362d8fae1dff1a904665698
munkhuu48@gmail.com <munkhuu48@gmail.com>
2018-03-27 18:31:35 -0700

    put required to form inputs

--- appbooker/forms.py
+++ appbooker/forms.py
@@ -3,8 +3,11 @@ from .models import Appointment
 from django.contrib.admin import widgets
 
 class AppointmentForm(forms.ModelForm):
-	datetime = forms.DateTimeField(label='', widget=widgets.AdminSplitDateTime())
+	datetime = forms.DateTimeField(label='', widget=widgets.AdminSplitDateTime(attrs={'required': 'true'}))
 	
 	class Meta:
 		model = Appointment
 		fields = ['datetime', 'desc']
+		widgets = {
+			'desc': forms.TextInput(attrs={'required': 'true'})
+		}
